b96229aeacf586f81ebd9ad17cf31cbf345e37d4
sekongo14 <sekongoismael0@gmail.com>
2024-06-05 22:56:52 +0000

    inscription api

--- account/views.py
+++ account/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .serializer import UserSerializer
+
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    print(queryset)
+    serializer_class = UserSerializer
