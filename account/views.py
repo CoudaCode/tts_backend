e2d159ffb1d11c8aec2808829d922a281d26095a
WOLLE JR <arnaudjuniorwolle003@gmail.com>
2024-06-22 03:32:06 +0000

    Profile

--- account/views.py
+++ account/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 from .serializer import *
+from django.http import JsonResponse
 
 User = get_user_model()
 
@@ -25,8 +26,19 @@ class ProfilUser(APIView):
 
 #Creation de la vue qui permettra de recuperer les info utilisateur
 class UserProfileView(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    def get(self, request, user_id):
+        try:
+            user = User.objects.get(pk=user_id)
+            user_data = {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                # Ajoutez d'autres champs que vous souhaitez exposer
+            }
+            return JsonResponse(user_data, status=200)
+        except User.DoesNotExist:
+            return JsonResponse({"error": "User not found"}, status=404)
     
 
