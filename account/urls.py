5e566138777dde56b5607116349e4f2c7f2655f0
Diarassouba Mamadou <119653418+CoudaCode@users.noreply.github.com>
2024-06-23 04:13:55 +0000

    commit

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import *
-from rest_framework_simplejwt.views import (
+from rest_framework_simplejwt.views import ( # type: ignore
     TokenObtainPairView,
     TokenRefreshView,
 )
